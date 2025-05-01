a944683df37e14d4fd53a3215a16aa49403de20c
Omkar-Abhang <omkarabhang36@gmail.com>
2025-05-01 21:47:26 +0530

    corection in uvicorn host

--- blogi_backend/main.py
+++ blogi_backend/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from app.routers import users, posts
 from app.database import Base, engine
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import os
 
 Base.metadata.create_all(bind=engine)
 
@@ -15,6 +17,14 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+if __name__ == "__main__":
+        uvicorn.run(
+            "main:app",
+            host="0.0.0.0",
+            port=int(os.environ.get("PORT", 8000)),  # important for Render
+            reload=True
+    )
 
 app.include_router(users.router)
 app.include_router(posts.router)
